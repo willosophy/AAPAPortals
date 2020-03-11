a97fc888209318ed2590244ed78b38adeb057727
Paul <paul.jbre@gmail.com>
2020-03-11 21:03:17 +0000

    Merged PR 8: Implement data import and hand multiple managed dependencies form vendor with data.

--- pipelines/powershell/import-data.ps1
+++ pipelines/powershell/import-data.ps1
@@ -1,11 +1,39 @@
+[CmdletBinding()]
+param (
+    $serverUrl,
+    $userName,
+    $userPassword,
+    $dataFile
+)
 
+Write-Host "Importing data from $($dataFile) to $($serverUrl)"
 
-if (Get-Module -ListAvailable -Name SomeModule) {
-    Write-Debug "Module exists"
-    Import-Module Microsoft.Xrm.Data.PowerShell
+if (Get-Module -ListAvailable -Name Microsoft.Xrm.Data.PowerShell ) {
+    Write-Host "Microsoft.Xrm.Data.PowerShell module exists skipping install."
+    Import-Module Microsoft.Xrm.Data.PowerShell -Force
 } 
 else {
-    Write-Debug "Module does not exist"
-    Install-Module Microsoft.Xrm.Data.PowerShell -Force
-    Import-Module Microsoft.Xrm.Data.PowerShell
+    Write-Host "Microsoft.Xrm.Data.PowerShell module does not exist installing now."
+    Install-Module Microsoft.Xrm.Data.PowerShell -Force 
+    Import-Module Microsoft.Xrm.Data.PowerShell -Force 
 }
+
+if (Get-Module -ListAvailable -Name Microsoft.Xrm.Tooling.ConfigurationMigration ) {
+    Write-Host "Microsoft.Xrm.Tooling.ConfigurationMigration module exists skipping install."
+    Import-Module Microsoft.Xrm.Tooling.ConfigurationMigration -Force 
+} 
+else {
+    Write-Host "Microsoft.Xrm.Tooling.ConfigurationMigration module does not exist installing now."
+    Install-Module -Name Microsoft.Xrm.Tooling.ConfigurationMigration -Force
+    Import-Module Microsoft.Xrm.Tooling.ConfigurationMigration -Force
+}
+
+# Convert to SecureString
+#[securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
+[pscredential] $credOject = New-Object System.Management.Automation.PSCredential ($userName, (ConvertTo-SecureString $userPassword -AsPlainText -Force)) -Verbose
+
+$connection = Connect-CrmOnline -ServerUrl $serverUrl -Credential $credOject
+Write-Host "Now connected to $($connection.ConnectedOrgFriendlyName)"
+
+Write-Host "Beginning data import..."
+Import-CrmDataFile -CrmConnection $connection -DataFile $dataFile -Verbose
