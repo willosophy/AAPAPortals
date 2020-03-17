94b94570e98b567c806db0239080aec84c64409e
Paul <paul.jbre@gmail.com>
2020-03-17 20:44:13 +0000

    Merged PR 11: Refactor pipelines to allow parallel deploy and add ability to set version number as part of build process

--- pipelines/powershell/set-solution-version.ps1
+++ pipelines/powershell/set-solution-version.ps1
@@ -1,9 +1,25 @@
 ﻿# Update solution version to current date and time
 [CmdletBinding()]
 param (
-    $solutionXmlFullPath
+    [String]$solutionXmlFullPath,
+    [String]$versionNumber
 )
 
+Write-Host "File path: $solutionXmlFullPath"
+$isValidPath = Test-Path -Path $solutionXmlFullPath
+
+Write-Host "Is valid path: $isValidPath"
+
 [xml]$xml = Get-Content $solutionXmlFullPath
-$myXml.ImportExportXml.SolutionManifest.Version = [string](get-date -Format yyyy.MM.dd.HHmmss)
-$myXml.Save($solutionXmlFullPath)
+
+if (-not ([string]::IsNullOrEmpty($versionNumber)))
+{
+    Write-Host "Custom verion set to: $versionNumber"
+    $xml.ImportExportXml.SolutionManifest.Version = $versionNumber
+} else {
+    $version = [string](get-date -Format yyyy.MM.dd.HHmmss)
+    Write-Host "Verion will be: $version"
+    $xml.ImportExportXml.SolutionManifest.Version = $version
+}
+
+$xml.Save($solutionXmlFullPath)
